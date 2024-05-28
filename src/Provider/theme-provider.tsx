0cfece28019192c3d442d7a92cee254d7fdfa022
Om Shah <omshah@Oms-Laptop.local>
2024-05-27 22:00:42 -0700

    Fix: Theme Issue

--- src/Provider/theme-provider.tsx
+++ src/Provider/theme-provider.tsx
@@ -6,7 +6,7 @@ import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-  <NextThemesProvider {...props}>
+  <NextThemesProvider {...props} defaultTheme="dark">
     {children}
   </NextThemesProvider>
   )
